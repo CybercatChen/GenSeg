13cd45e4c039478efa91bfb5d79c4cfcb9c9dd7e
ChenSQ <chensiqi@bjfu.edu.cn>
2024-05-07 16:30:59 +0800

    update gen and seg

---
+++ generate/submit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -J point       # job name, optional
+#SBATCH -N 1          # number of computing node
+#SBATCH -c 5          # number of cpus, for multi-thread programs
+#SBATCH --gres=gpu:1  # number of gpus allocated on each node
+#SBATCH -w node05
+
+python train_gen.py
