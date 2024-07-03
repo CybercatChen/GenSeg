3adbdb7ed4fd3db0a69ee4fb6f80749c14138ed9
ChenSQ <chensiqi@bjfu.edu.cn>
2024-07-03 12:54:00 +0800

    update and rebuild

--- logs/submit_cls.sh
+++ logs/submit_cls.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH -J chair       # job name, optional
+#SBATCH -J cls       # job name, optional
 #SBATCH -N 1          # number of computing node
 #SBATCH -c 5          # number of cpus, for multi-thread programs
 #SBATCH --gres=gpu:1  # number of gpus allocated on each node
 #SBATCH -w node04
-#SBATCH --qos=high
 
-python train_seg.py
+python train_cls.py
